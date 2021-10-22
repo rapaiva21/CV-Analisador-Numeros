d85537501958a9077dd8a0831ce9f1f752ef2703
Carlos Henrique <rapaiva21@gmail.com>
2021-10-21 23:30:31 -0300

    Numeros adicionados em um array

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var numeros = []
 function adicionar(){
-    alert('teste de botão')
+    let num = document.getElementById('txtn')
+    let number = Number(num.value)
+    if (number == 0 || number > 100 || number.length == 0){
+        alert('ERRO ao adicionar os dados. Tente outra vez')
+    } else {
+        console.log('accept')
+        numeros.push(number)
+        console.log(numeros)
+        num.value = ''
+    }
 }
